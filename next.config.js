c4ac26cc79126ce9b5cb497b61a9adf7b96b04e3
Erasyl Kokenov <kokenaoerasyl@gmail.com>
2024-10-21 17:20:53 +0500

    updated config for cache

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
 // next.config.js
 module.exports = {
+    async headers() {
+        return [
+            {
+                source: '/images/(.*)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0'
+                    },
+                    {
+                        key: 'Pragma',
+                        value: 'no-cache'
+                    },
+                    {
+                        key: 'Expires',
+                        value: '0'
+                    }
+                ]
+            }
+        ];
+    },
     async rewrites() {
         return [
             {
