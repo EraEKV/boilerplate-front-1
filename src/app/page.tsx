5cc0fe4308d08478bf429aed75b5e834528d6e7a
Erasyl Kokenov <kokenaoerasyl@gmail.com>
2024-10-21 16:55:43 +0500

    RequestForm updated and page remove padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import RequestForm from "./widgets/RequestFrom";
 
 export default function Home() {
   return (
-    <div className="bg-white min-h-screen px-10 pt-32 space-y-20">
+    <div className="bg-white min-h-screen pt-32 space-y-20">
         <Header />
         <DropFile />
 
--- src/app/widgets/RequestFrom.tsx
+++ src/app/widgets/RequestFrom.tsx
@@ -29,25 +29,27 @@ const RequestForm = () => {
     ];
 
     return (
-        <div className="flex flex-col justify-center items-center w-full min-h-screen">
-            <div className="bg-white shadow-md rounded-lg p-8 w-96 space-y-4">
-                <h1 className="text-2xl font-semibold text-center">Что вы хотите сгенерировать?</h1>
-                <input
-                    type="text"
-                    value={query}
-                    onChange={handleChange}
-                    placeholder="Введите запрос..."
-                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-400"
-                />
-                <button
-                    onClick={handleGenerateClick}
-                    className="w-full p-2 text-white rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 hover:bg-gradient-to-r hover:from-green-400 hover:to-yellow-400 transition duration-300 ease-in-out"
-                >
-                    Сгенерировать
-                </button>
+        <div className="px-4 flex flex-col bg-bg justify-center items-center w-full min-h-screen">
+            <div className="bg-white shadow-md rounded-lg px-8 py-8 max-w-96 space-y-14">
+                <h1 className="text-2xl font-semibold text-center">Что вы хотите создать?</h1>
+                <div className="space-y-4">
+                    <input
+                        type="text"
+                        value={query}
+                        onChange={handleChange}
+                        placeholder="Введите запрос..."
+                        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-400"
+                    />
+                    <button
+                        onClick={handleGenerateClick}
+                        className="w-full py-3 text-white text-lg font-bold rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 hover:shadow-lg transition-shadow duration-300 ease-in-out"
+                    >
+                        Сгенерировать
+                    </button>
+                </div>
             </div>
 
-            <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-4xl px-4">
+            <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-4xl">   
                 {suggestions.map((suggestion, index) => (
                     <div
                         key={index}
